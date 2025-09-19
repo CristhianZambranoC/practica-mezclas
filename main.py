2ba77a701afd1ddea64eda945cfee1348d913948
TEBANSour <estebanortiz1804@gmail.com>
2025-09-19 08:28:47 -0500

    nose

--- main.py
+++ main.py
@@ -1,13 +1,9 @@
 nombre="Mundo"
 print("Hola  " + nombre)
-while nombre == "Mundo":
-    print(32+65*56)
+
 
 print("Soy ESTeban")
 print("Soy Alejandra")
-print("Soy Cristhian")
-
 print("Mezcla 1 Cristhian")
 
-
-print("^.^")
+print("nose")
