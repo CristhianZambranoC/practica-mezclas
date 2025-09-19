d5a0a51678753778e1fe17e4c46a71bc399ba27f
TEBANSour <estebanortiz1804@gmail.com>
2025-09-19 08:16:09 -0500

    ciclo while

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 nombre="Mundo"
 print("Hola  " + nombre)
+while nombre == "Mundo":
+    print(32+65*56)
 
-<<<<<<< HEAD
 print("Soy ESTeban")
-=======
 print("Soy Alejandra")
 print("Soy Cristhian")
->>>>>>> bf2b0fc04cfc6273625b0a48049b130c2917220d
